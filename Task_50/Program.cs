52816ba10dff69edacb8481d257bf9bef4635c86
Den <denis-s2@yandex.ru>
2022-10-07 14:15:10 +0500

    homework is completed

---
+++ Task_50/Program.cs
@@ -0,0 +1,73 @@
+﻿using static System.Console;
+Clear();
+// Напишите программу, которая на вход принимает 
+// позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента 
+// или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+WriteLine("Задайте размер массива");
+WriteLine("Строки: ");
+int rows = int.Parse(ReadLine()!);
+WriteLine("Столбцы: ");
+int col = int.Parse(ReadLine()!);
+WriteLine();
+
+WriteLine("Задайте позицию элемента для проверки:");
+WriteLine("Строка: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Столбец: ");
+int b = int.Parse(ReadLine()!);
+if (a > rows || b > col)
+{
+    WriteLine("Такой позиции нет");
+    return;
+}
+a -= 1;
+b -= 1;
+int[,] matrix = new int[rows, col];
+FillRandomNumbers(matrix);
+PrintArray(matrix);
+WriteLine();
+
+
+CheckElement(matrix, a, b);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillRandomNumbers(int[,] matr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matr;
+}
+
+void CheckElement(int[,] matr, int x, int y)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (i == x && j == y) WriteLine(matr[i, j]);
+        }
+    }
+}
---
+++ Task_52/Program.cs
@@ -0,0 +1,59 @@
+﻿using static System.Console;
+Clear();
+//  1. Задайте двумерный массив из целых чисел. 
+//  2. Найдите среднее арифметическое элементов в каждом столбце.
+//  Например, задан массив:
+//  1 4 7 2
+//  5 9 2 3
+//  8 4 2 4
+//  Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; i.
+WriteLine("Задайте размер массива");
+WriteLine("Строки: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Столбцы: ");
+int b = int.Parse(ReadLine()!);
+int[,] arr = CreateArray(a, b);
+PrintArray(arr);
+WriteLine();
+ColumnsSum(arr);
+
+int[,] CreateArray(int rows, int col)
+
+{
+    Random rnd = new Random();
+    int[,] array = new int[rows, col];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ColumnsSum(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum += array[j, i];
+        }
+        sum /= array.GetLength(0);
+        Write($"{sum:f1}; ");
+    }
+}
